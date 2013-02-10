3ff30ff8097d908a791aff0e0b245d51cfe8c599
pawel.gdula <gdula.pawel@gmail.com>
2013-02-10 17:58:37 +0100

    Crawler for Pudelek ...

--- src/groovy/vanity/ContentSource.groovy
+++ src/groovy/vanity/ContentSource.groovy
@@ -2,11 +2,20 @@ package vanity
 
 public enum ContentSource {
 
-    PUDELEK('www.pudelek.pl')
+    PUDELEK('http://www.pudelek.pl/')
 
     final String address
 
     ContentSource(String address) {
         this.address = address
+    }
+
+    public String getTriggerName(){
+        return this.name()
+    }
+
+    @Override
+    public String toString() {
+        return this.name()
     }
 }
