03e2629bb7947520c07958d539d0b21fc4c8072e
pawel.gdula <gdula.pawel@gmail.com>
2013-02-21 01:07:25 +0100

    - external configuration

---
+++ src/groovy/vanity/utils/ConfigUtils.groovy
@@ -0,0 +1,31 @@
+package vanity.utils
+
+import grails.util.Environment
+import groovy.util.logging.Log4j
+
+@Log4j
+@Singleton
+class ConfigUtils {
+
+    public List<String> externalConfig(final def grails, final File userHome, final Closure dataCollector){
+        // prepare list in paths to external config will be located
+        List<String> files = []
+        // enhance setter class to support "file" method
+        dataCollector.metaClass.file = {final String fileName ->
+            // build path to the file
+            String configFilePath = "${userHome}/.grails/vanity/${Environment.current}/${fileName}"
+            // validate existence of config file
+            if (!(new File(configFilePath).exists())){
+                throw new IllegalArgumentException("Cant find config file: ${configFilePath}")
+            }
+            // accumulate path as external file
+            log.info("Loading external file: ${configFilePath}")
+            files << "file:${configFilePath}"
+        }
+        // evaluate data collector
+        dataCollector.call()
+        // assign collected files as external resources
+        grails.config.locations = files
+    }
+
+}
