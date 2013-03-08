14f25f0b6cda03c17c69ca3e07e5f0cff01527a1
pawel.gdula <gdula.pawel@gmail.com>
2013-03-08 02:02:29 +0100

    - solr integration - indexer

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,12 +1,36 @@
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+// uncomment (and adjust settings) to fork the JVM to isolate classpaths
+//grails.project.fork = [
+//   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
+//]
 
 grails.project.dependency.resolution = {
     inherits("global") {}
-    log "warn"
-    legacyResolve
-    repositories {}
-    dependencies {}
-    plugins {}
+    log "error"          // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true       // Whether to verify checksums on resolve
+    legacyResolve false  // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+
+    repositories {
+        inherits true    // Whether to inherit repository definitions from plugins
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+    }
+
+    dependencies {
+    }
+
+    plugins {
+    }
 }
+
+
