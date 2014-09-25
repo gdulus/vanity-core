9127ba76ca929ff963c6a34acf6ad5cb698b364c
pawel.gdula <gdula.pawel@gmail.com>
2014-09-26 00:50:34 +0200

    - removes search resource - removes tracking - integration with external tracking api

--- grails-app/services/vanity/tracking/ClickService.groovy
+++ grails-app/services/vanity/tracking/ClickService.groovy
@@ -1,27 +1,8 @@
 package vanity.tracking
 
-import org.apache.commons.lang.Validate
-import org.springframework.scheduling.annotation.Async
-import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
-import vanity.article.Article
-import vanity.article.Tag
 
 class ClickService {
-
-    @Async
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void create(final Article article) {
-        Validate.notNull(article, 'Provide not null article object')
-        ArticleClick.withNewSession { new ArticleClick(article: article).save(flush: true) }
-    }
-
-    @Async
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void create(final Tag tag) {
-        Validate.notNull(tag, 'Provide not null tag object')
-        TagClick.withNewSession { new TagClick(tag: tag).save(flush: true) }
-    }
 
     @Transactional
     public Integer deleteAllByArticleIds(final Set<Long> ids) {
