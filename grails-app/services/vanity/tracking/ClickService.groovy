2d0f551265f494f83a15d76854eb1ae2c4311115
pawel.gdula <gdula.pawel@gmail.com>
2015-01-07 17:07:45 +0100

    - new header - tracking through ajax

--- grails-app/services/vanity/tracking/ClickService.groovy
+++ grails-app/services/vanity/tracking/ClickService.groovy
@@ -1,8 +1,25 @@
 package vanity.tracking
 
+import org.apache.commons.lang.Validate
 import org.springframework.transaction.annotation.Transactional
+import vanity.article.Article
+import vanity.article.Tag
 
 class ClickService {
+
+    @Transactional
+    public void createForArticle(final Long id) {
+        Validate.notNull(id, 'Provide not null article id')
+        Article article = Article.load(id)
+        new ArticleClick(article: article).save(flush: true)
+    }
+
+    @Transactional
+    public void createForTag(final Long id) {
+        Validate.notNull(id, 'Provide not null tag id')
+        Tag tag = Tag.load(id)
+        new TagClick(tag: tag).save(flush: true)
+    }
 
     @Transactional
     public Integer deleteAllByArticleIds(final Set<Long> ids) {
