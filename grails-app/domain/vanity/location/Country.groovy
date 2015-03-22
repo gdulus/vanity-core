af83b483b4567d2676cb33a59dee015c20ab671c
pawel.gdula <gdula.pawel@gmail.com>
2015-03-22 16:09:56 +0100

    - Countries job admin

--- grails-app/domain/vanity/location/Country.groovy
+++ grails-app/domain/vanity/location/Country.groovy
@@ -4,11 +4,14 @@ class Country {
 
     String name
 
+    String isoCode
+
     Date dateCreated
 
     Date lastUpdated
 
     static constraints = {
-        name(nullable: false, blank: false, maxSize: 250, unique: true)
+        name(nullable: false, blank: false, maxSize: 250)
+        isoCode(nullable: false, blank: false, maxSize: 10, unique: true)
     }
 }
---
+++ grails-app/services/vanity/location/CountryService.groovy
@@ -0,0 +1,56 @@
+package vanity.location
+
+import org.springframework.transaction.annotation.Transactional
+import vanity.celebrity.Job
+import vanity.pagination.PaginationAware
+import vanity.pagination.PaginationBean
+import vanity.pagination.PaginationParams
+
+class CountryService implements PaginationAware<Job> {
+
+    @Transactional(readOnly = true)
+    public Country read(final Long id) {
+        return Country.read(id)
+    }
+
+    @Transactional(readOnly = true)
+    PaginationBean<Country> listWithPagination(final PaginationParams params) {
+        return new PaginationBean<Country>(Country.list(max: params.max, offset: params.offset, sort: params.sort), Country.count())
+    }
+
+    @Transactional
+    Country save(final String name, final String isoCode) {
+        Country country = new Country(name: name, isoCode: isoCode)
+        country.save()
+        return country
+    }
+
+    @Transactional
+    Country update(final Long id, final String name, final String isoCode) {
+        Country country = Country.get(id)
+
+        if (!country) {
+            return null
+        }
+
+        if (name) {
+            country.name = name
+        }
+
+        if (isoCode) {
+            country.isoCode = isoCode
+        }
+
+        country.save()
+        return country
+    }
+
+    @Transactional
+    void delete(final Long id) {
+        Country country = Country.get(id)
+
+        if (country) {
+            country.delete()
+        }
+    }
+}
